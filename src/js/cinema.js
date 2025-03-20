13da9fee4729f4d66fb1cdbe39717fa393805391
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2025-03-20 17:36:00 -0400

    carrossel

--- src/js/cinema.js
+++ src/js/cinema.js
@@ -1,19 +1,187 @@
-// const items = document.querySelector('.items');
-// let index = 0;
-// const totalSlides = document.querySelectorAll('.item').length;
+        // Carrossel 1: Direção para a direita e com uma velocidade mais rápida
+        const swiper1 = new Swiper('#carousel1', {
+            loop: true, // Habilita o loop infinito
+            slidesPerView: 3, // Exibe 3 imagens ao mesmo tempo
+            spaceBetween: 5, // Reduz o espaço entre as imagens
+            autoplay: {
+                delay: 0, // Sem delay, rotação contínua
+                disableOnInteraction: false, // O autoplay não será desativado após a interação
+            },
+            speed: 5000, // Velocidade da transição entre os slides (mais rápida)
+            slidesPerGroup: 1, // Faz com que cada slide seja exibido um por vez
+            effect: 'slide', // Efeito de transição suave
+            direction: 'horizontal', // Direção para a direita
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                1024: {
+                    slidesPerView: 3,
+                },
+            },
+        });
 
-// function moveCarrossel() {
-//     index++;
-//     if (index === totalSlides) {
-//         setTimeout(() => {
-//             items.style.transition = "none"; 
-//             items.style.transform = "translateX(0)"; 
-//             index = 0;
-//         }, 500); // Tempo da transição antes de resetar
-//     } else {
-//         items.style.transition = "transform 0.5s ease-in-out"; 
-//         items.style.transform = `translateX(-${index * 100}%)`;
-//     }
-// }
+        // Carrossel 2: Direção para a esquerda e com uma velocidade mais lenta
+        const swiper2 = new Swiper('#carousel2', {
+            loop: true, // Habilita o loop infinito
+            slidesPerView: 3, // Exibe 3 imagens ao mesmo tempo
+            spaceBetween: 5, // Reduz o espaço entre as imagens
+            autoplay: {
+                delay: 0, // Sem delay, rotação contínua
+                disableOnInteraction: false, // O autoplay não será desativado após a interação
+            },
+            speed: 8000, // Velocidade da transição entre os slides (mais lenta)
+            slidesPerGroup: 1, // Faz com que cada slide seja exibido um por vez
+            effect: 'slide', // Efeito de transição suave
+            direction: 'horizontal', // Direção para a esquerda
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                1024: {
+                    slidesPerView: 3,
+                },
+            },
+        });
 
-// setInterval(moveCarrossel, 3000); // Tempo entre cada slide (3 segundos)
+        const swiper3 = new Swiper('#carousel3', {
+            loop: true, // Habilita o loop infinito
+            slidesPerView: 3, // Exibe 3 imagens ao mesmo tempo
+            spaceBetween: 5, // Reduz o espaço entre as imagens
+            autoplay: {
+                delay: 0, // Sem delay, rotação contínua
+                disableOnInteraction: false, // O autoplay não será desativado após a interação
+            },
+            speed: 5000, // Velocidade da transição entre os slides (mais lenta)
+            slidesPerGroup: 1, // Faz com que cada slide seja exibido um por vez
+            effect: 'slide', // Efeito de transição suave
+            direction: 'horizontal', // Direção para a esquerda
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                1024: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+
+        const swiper4 = new Swiper('#carousel4', {
+            loop: true, // Habilita o loop infinito
+            slidesPerView: 3, // Exibe 3 imagens ao mesmo tempo
+            spaceBetween: 5, // Reduz o espaço entre as imagens
+            autoplay: {
+                delay: 0, // Sem delay, rotação contínua
+                disableOnInteraction: false, // O autoplay não será desativado após a interação
+            },
+            speed: 8000, // Velocidade da transição entre os slides (mais lenta)
+            slidesPerGroup: 1, // Faz com que cada slide seja exibido um por vez
+            effect: 'slide', // Efeito de transição suave
+            direction: 'horizontal', // Direção para a esquerda
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                1024: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+
+        const swiper5 = new Swiper('#carousel5', {
+            loop: true, // Habilita o loop infinito
+            slidesPerView: 3, // Exibe 3 imagens ao mesmo tempo
+            spaceBetween: 5, // Reduz o espaço entre as imagens
+            autoplay: {
+                delay: 0, // Sem delay, rotação contínua
+                disableOnInteraction: false, // O autoplay não será desativado após a interação
+            },
+            speed: 5000, // Velocidade da transição entre os slides (mais lenta)
+            slidesPerGroup: 1, // Faz com que cada slide seja exibido um por vez
+            effect: 'slide', // Efeito de transição suave
+            direction: 'horizontal', // Direção para a esquerda
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                1024: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+
+
+        const swiper6 = new Swiper('#carousel6', {
+            loop: true, // Habilita o loop infinito
+            slidesPerView: 3, // Exibe 3 imagens ao mesmo tempo
+            spaceBetween: 5, // Reduz o espaço entre as imagens
+            autoplay: {
+                delay: 0, // Sem delay, rotação contínua
+                disableOnInteraction: false, // O autoplay não será desativado após a interação
+            },
+            speed: 8000, // Velocidade da transição entre os slides (mais lenta)
+            slidesPerGroup: 1, // Faz com que cada slide seja exibido um por vez
+            effect: 'slide', // Efeito de transição suave
+            direction: 'horizontal', // Direção para a esquerda
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                1024: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+
+        const swiper7 = new Swiper('#carousel7', {
+            loop: true, // Habilita o loop infinito
+            slidesPerView: 3, // Exibe 3 imagens ao mesmo tempo
+            spaceBetween: 5, // Reduz o espaço entre as imagens
+            autoplay: {
+                delay: 0, // Sem delay, rotação contínua
+                disableOnInteraction: false, // O autoplay não será desativado após a interação
+            },
+            speed: 5000, // Velocidade da transição entre os slides (mais lenta)
+            slidesPerGroup: 1, // Faz com que cada slide seja exibido um por vez
+            effect: 'slide', // Efeito de transição suave
+            direction: 'horizontal', // Direção para a esquerda
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                1024: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+
+
+        // Forçar o Swiper a não dar "pause" (importante para a continuidade sem interrupção)
+        swiper1.on('slideChangeTransitionEnd', function() {
+            swiper1.autoplay.start();
+        });
+
+        swiper2.on('slideChangeTransitionEnd', function() {
+            swiper2.autoplay.start();
+        });
